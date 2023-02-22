bf854a2b5749af162a82649901769fac41ac5883
David Arredondo <davidarredondolopez@gmail.com>
2023-02-22 13:54:12 -0600

    variables, comentarios y console logs

--- Javascript Moderno/01-FUNDAMENTOS/app.js
+++ Javascript Moderno/01-FUNDAMENTOS/app.js
@@ -13,6 +13,19 @@ c=20; no se puede hacer
 comentario multilinea
  
 */
-let a=10,b=10,c=10,d=10;
+let a=10,
+b=20,
+c=30,
+d=10;
 let x = a+b;
-console.log(x);
+console.log(x); //log normal
+console.warn(x); // warning
+console.error(x); // error
+// enviamos la variable como objeto
+//Enviamos estilos de css a la consola
+console.log('%c Mis variables son','color: darkblue;font-weight:bold');
+console.log({a});
+console.log({b});
+console.log({c});
+
+console.table({a,b,c,d,x}); // permite mostrar el objeto como tabla
