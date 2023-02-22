82c7dc34055adc9d43c8d2d305bf789f96e0ed60
David Arredondo <davidarredondolopez@gmail.com>
2023-02-22 13:39:24 -0600

    Variables y comentarios

--- Javascript Moderno/01-FUNDAMENTOS/app.js
+++ Javascript Moderno/01-FUNDAMENTOS/app.js
@@ -1,1 +1,18 @@
-console.log('Hola Mundo');
+// console.log('Hola Mundo');
+
+// Formas para crear variables
+
+// let a = 10;
+// var b= 10;
+// const c=10; 
+
+/* 
+const no cambian de valor, son constantes
+c=20; no se puede hacer
+
+comentario multilinea
+ 
+*/
+let a=10,b=10,c=10,d=10;
+let x = a+b;
+console.log(x);
